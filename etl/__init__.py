450dcdbbe1caa44fd1fa35e0902321e090ee1c32
Lucas Passos <lspassos@icloud.com>
2026-06-01 20:37:46 +0100

    fix(etl): support Python 3.9 datetime UTC
    Refs #2

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,2 +1,8 @@
 """ETL package for Radar da Violencia Brasil."""
 
+import datetime as _datetime
+
+
+# Python 3.10 and older do not expose datetime.UTC.
+if not hasattr(_datetime, "UTC"):
+    _datetime.UTC = _datetime.timezone.utc
